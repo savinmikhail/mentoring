c69ec58906b35b15d4ea44549a212f3d00090869
aldar <aldar.debeev@yandex.ru>
2023-08-08 15:17:20 +0700

    fix compiler

--- app/Http/Controllers/CompilerController.php
+++ app/Http/Controllers/CompilerController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 
 class CompilerController extends Controller
 {
-   public function compile()
+   public function compile(Request $request)
    {
-       $code = $_POST['code'];
+       $code = $request->all()['code'];
+
        $filePath ="/var/www/html/storage/logs/code.php";
        file_put_contents($filePath, $code);
 
        $output = shell_exec("/usr/local/bin/php $filePath 2>&1");
+
        echo $output;
 
+//       return view('welcome', [
+//           'output' => 'sadasddasd',
+//       ]);
    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,27 +5,69 @@
     <title>Codeboard Online IDE</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body >
+<body>
 
-<div class="header"> Codeboard Online IDE </div>
-<form id="csrf">
-    @csrf
-    <div class="editor" id="editor"></div>
-</form>
+<div class="split-screen-container">
+    <div class="theory-container">
+        <!-- Добавьте ваш контент с теорией здесь -->
+        <!-- Например: -->
+        <h2>PHP: Условная конструкция (if)</h2>
+        <p>Задача предиката — получить ответ на вопрос, но обычно этого недостаточно и нужно выполнить определенное действие в зависимости от ответа.
+            Напишем функцию, которая определяет тип переданного предложения. Для начала она будет отличать обычные предложения от вопросительных: </p>
+    </div>
+    <div class="editor-container">
+        <div class="header"> PHP Online IDE </div>
+        <form id="csrf">
+            @csrf
+            <div class="editor" id="editor"></div>
+        </form>
+        <div class="button-container">
+            <button class="btn" onclick="executeCode()"> Запустить </button>
+        </div>
 
 
-<div class="button-container">
-    <button class="btn" onclick="executeCode()"> Run </button>
+        <div class="output"></div>
+
+    </div>
 </div>
-
-<div class="output"></div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="js/lib/ace.js"></script>
-<script src="js/lib/theme-monokai.js"></script>
-<script src="js/ide.js"></script>
+
+
+<script >
+    let editor;
+
+    window.onload = function() {
+        editor = ace.edit("editor");
+        editor.setTheme("ace/theme/twilight");
+        // editor.$highlightBrackets(true);
+        editor.setFontSize(26);
+        editor.session.setMode("ace/mode/php");
+    }
+
+    function executeCode() {
+
+        $.ajax({
+
+            url: "/com",
+
+            method: "POST",
+
+            data: {
+                code: editor.getSession().getValue(),
+                "_token": "{{ csrf_token() }}",
+            },
+
+            success: function(response) {
+                $(".output").text(response)
+            }
+        })
+
+    }
+</script>
 
 </body>
 </html>
