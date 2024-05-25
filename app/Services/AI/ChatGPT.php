1124148774ada228566184452eea42e1ef7f925a
Savin Mikhail <mikhail.d.savin@gmail.com>
2024-05-25 17:04:26 +0700

    fix: file permissions

--- app/Services/AI/ChatGPT.php
+++ app/Services/AI/ChatGPT.php
@@ -1,23 +1,43 @@
 <?php
 
 namespace App\Services\AI;
+use Exception;
 use Orhanerday\OpenAi\OpenAi;
 
 class ChatGPT
 {
-
-
-public function chat($prompt)
+    /**
+     * @throws Exception
+     */
+    public function chat(string $prompt): array
 {
     $open_ai = new OpenAi(env('OPEN_AI_KEY'));
+    $open_ai->setBaseURL('https://api.proxyapi.ru/openai');
 
-    $complete = $open_ai->completion([
-        'model' => 'text-davinci-003',
-        'prompt' => ' '. $prompt,
-        'temperature' => 0.9,
-        'max_tokens' => 150,
+    $complete = $open_ai->chat([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            [
+                "role" => "system",
+                "content" => "You are a helpful assistant."
+            ],
+            [
+                "role" => "user",
+                "content" => "Who won the world series in 2020?"
+            ],
+            [
+                "role" => "assistant",
+                "content" => "The Los Angeles Dodgers won the World Series in 2020."
+            ],
+            [
+                "role" => "user",
+                "content" => "Where was it played?"
+            ],
+        ],
+        'temperature' => 1.0,
+        'max_tokens' => 4000,
         'frequency_penalty' => 0,
-        'presence_penalty' => 0.6,
+        'presence_penalty' => 0,
     ]);
 
     $response = json_decode($complete, true);
